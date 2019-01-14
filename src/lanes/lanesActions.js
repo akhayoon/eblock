bdee3155649e0b48c073f3af69bfecc920ac97ef
akhayoon <akhayoon@gmail.com>
2019-01-14 13:39:40 -0500

    fixed spacing issues

--- src/lanes/lanesActions.js
+++ src/lanes/lanesActions.js
@@ -17,23 +17,23 @@ export function changeSearchText(searchText) {
 // A thunk is overkill, but I just want to show how I usually handle async actions
 export function fetchLanes(formData)
 {
-	return (dispatch, getState) => {
+  return (dispatch, getState) => {
     // Please not that these actions REQUEST/SUCCESS/FAILURE are for reference/logging 
     // purposes only and are not used in the reducer.
-		dispatch({type: types.FETCH_LANES_REQUEST});
+    dispatch({type: types.FETCH_LANES_REQUEST});
 
     // This try block is used to control errors that might occur in async actions.
-		try {
+    try {
       const payload = require('../payload.json');
       dispatch({type: types.FETCH_LANES_SUCCESS, payload});
       
       const {data} = payload;
-			dispatch({type: types.RECEIVE_LANES, lanes: data.lanes});
-		} catch(error) {
-			dispatch({type: types.FETCH_LANES_FAILURE});
+      dispatch({type: types.RECEIVE_LANES, lanes: data.lanes});
+    } catch(error) {
+      dispatch({type: types.FETCH_LANES_FAILURE});
 
       // Report this error here to a reporter like Sentry or LogRocket
       // You should also trigger an action to show something to user on the view.
     }
-	}
+  }
 }
