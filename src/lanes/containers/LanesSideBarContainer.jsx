58c0ed6c3b2815a70e10a5439f8bbd312641fe6e
Hussien Khayoon <hussien@vendorhero.com>
2019-01-14 01:26:56 -0500

    fixed typo on file name

--- src/lanes/containers/LanesSideBarContainer.jsx
+++ src/lanes/containers/LanesSideBarContainer.jsx
@@ -2,7 +2,7 @@ import React, { Component } 	from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import * as LanesActions from '../landesActions'
+import * as LanesActions from '../lanesActions'
 import LanesSideBarMain from '../components/LanesSideBarMain.jsx'
 import '../styles/lanes.scss'
 
